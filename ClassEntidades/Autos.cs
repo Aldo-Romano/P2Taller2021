2071e75103dc25a38e61183af0ce33789204655f
Aldo Romano <aldoromanorojas@gmail.com>
2021-08-03 04:01:35 -0500

    04.Modificacion en la Capa Entidades. Clases clientes y autos

--- ClassEntidades/Autos.cs
+++ ClassEntidades/Autos.cs
@@ -8,5 +8,12 @@ namespace ClassEntidades
 {
    public class Autos
     {
+        public int id_Auto { get; set; }
+        public int f_marca { get; set; }
+        public string modelo { get; set; }
+        public string año { get; set; }
+        public string color { get; set; }
+        public string placas { get; set; }
+        public int dueño { get; set; }
     }
 }
