a6040a44373d5bab61cba79366cc6535afaec4b8
Ceciah Ismari <ceciahisma@gmail.com>
2021-08-03 06:30:03 -0500

    14.Modificacion a la Capa de Entidades. Constructores de las clases Mecanico y Revisiones

--- ClassEntidades/Mecanicos.cs
+++ ClassEntidades/Mecanicos.cs
@@ -8,5 +8,11 @@ namespace ClassEntidades
 {
     public class Mecanicos
     {
+        public int id_Tecnico { get; set; }
+        public string Nombre { get; set; }
+        public string App { get; set; }
+        public string Apm { get; set; }
+        public string Celular { get; set; }
+        public string Correo { get; set; }
     }
 }
